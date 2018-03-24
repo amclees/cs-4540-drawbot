0391bba716fe0f10184480be1be63c37ba51b031
Andrew McLees <andrewmclees5@gmail.com>
2018-03-24 13:25:01 -0700

    Add arm lengths as a parameter to get_pos

--- draw.h
+++ draw.h
@@ -10,7 +10,7 @@ struct vector2 get_target_angles(struct vector2 target_pos, struct vector2 start
 
 struct vector2* get_interpolated_points(struct vector2 pos1, struct vector2 pos2);
 
-struct vector2 get_pos(struct vector2 ang);
+struct vector2 get_pos(struct vector2 ang, struct vector2 arm_len);
 
 void follow_path(struct vector2* path, int step);
 
