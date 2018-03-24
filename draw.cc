1543455a822cf7dff78e883fdf9f21b7f692be97
Andrew McLees <andrewmclees5@gmail.com>
2018-03-24 13:40:15 -0700

    Implement follow_path

--- draw.cc
+++ draw.cc
@@ -1,1 +1,9 @@
 // This file will implement the draw library functions
+#include "draw.h"
+
+void follow_path(vector2* path, int step, Stepper stepper1, Stepper stepper2) {
+  vector2 current = path[step * sizeof(vector2)];
+  stepper1.step(current[0]);
+  stepper2.step(current[1]);
+}
+
