bc90f1c19822aa8c05ef74f22a9b628b1458deb9
Andrew McLees <andrewmclees5@gmail.com>
2018-03-24 13:42:57 -0700

    Include length of path in find_path return value

--- draw.h
+++ draw.h
@@ -8,6 +8,11 @@ typedef struct {
   double y;
 } vector2;
 
+typedef struct {
+  vector2* points;
+  int length;
+} path;
+
 vector2 get_target_angles(vector2 target_pos, vector2 start_ang);
 
 vector2* get_interpolated_points(vector2 pos1, vector2 pos2);
@@ -16,6 +21,6 @@ vector2 get_pos(vector2 ang, vector2 arm_len);
 
 void follow_path(vector2* path, int step, Stepper stepper1, Stepper stepper2);
 
-vector2* find_path(vector2 start_ang, vector2 target_ang);
+path* find_path(vector2 start_ang, vector2 target_ang);
 
 #endif
