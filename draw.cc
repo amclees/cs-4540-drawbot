4d64d67e15790cb5ef5164af3c06e488a0b13115
Robin Chan <rchan22@calstatela.edu>
2018-03-24 14:45:47 -0700

    Implemented get_interpolated_points :3

--- draw.cc
+++ draw.cc
@@ -1,24 +1,8 @@
-// This file will implement the draw library functions
-#include "draw.h"
-#include "math.h"
-
-vector2 get_pos(vector2 ang, vector2 arm_len){
-    double x1 = arm_len1*cos(ang.x*M_PI/180);
-    double y1 = arm_len1*sin(ang.x*M_PI/180);
-    double x2 = arm_len2*cos((ang.x+ang.y)*M_PI/180);
-    double y2 = arm_len2*sin((ang.x+ang.y)*M_PI/180);
-    
-    vector2 result;
-    result.x = x1+x2;
-    result.y = y1+y2;
-    return result;
-}
-
 vector2* get_interpolated_points(vector2 pos1, vector2 pos2, int num_points)
 {
-    vector2* list = malloc(num_points * sizeof(vector2));
+	vector2* list = malloc(num_points * sizeof(vector2));
     
-    double x0 = pos1.x;
+	double x0 = pos1.x;
 	double x1 = pos2.x;
 	double y0 = pos1.y;
 	double y1 = pos2.y;
