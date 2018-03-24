e021b56b06b467589b5ca12c9650ee7ccecdae59
Andrew McLees <andrewmclees5@gmail.com>
2018-03-24 14:30:20 -0700

    Add whitespace to draw.cc
    Some may claim whitespace is useless, but it makes
    the code more readable long-term.

--- draw.cc
+++ draw.cc
@@ -26,10 +26,10 @@ path find_path(vector2 start_ang, vector2 target_ang) {
 }
 
 vector2 get_pos(vector2 ang, vector2 arm_len){
-    double x1 = arm_len1*cos(ang.x*M_PI/180);
-    double y1 = arm_len1*sin(ang.x*M_PI/180);
-    double x2 = arm_len2*cos((ang.x+ang.y)*M_PI/180);
-    double y2 = arm_len2*sin((ang.x+ang.y)*M_PI/180);
+    double x1 = arm_len1 * cos(ang.x * M_PI / 180);
+    double y1 = arm_len1 * sin(ang.x * M_PI / 180);
+    double x2 = arm_len2 * cos((ang.x + ang.y) * M_PI / 180);
+    double y2 = arm_len2 * sin((ang.x + ang.y) * M_PI / 180);
 
     vector2 result;
     result.x = x1+x2;
