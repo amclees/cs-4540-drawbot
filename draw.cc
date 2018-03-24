a30898f8c448b37d9245a558ee868e72d14a7ada
Andrew McLees <andrewmclees5@gmail.com>
2018-03-24 14:09:03 -0700

    Finish path functions

--- draw.cc
+++ draw.cc
@@ -1,9 +1,25 @@
 // This file will implement the draw library functions
 #include "draw.h"
+#include "math.h"
 
-void follow_path(vector2* path, int step, Stepper stepper1, Stepper stepper2) {
-  vector2 current = path[step * sizeof(vector2)];
+void follow_path(vector2* to_follow, int step, Stepper stepper1, Stepper stepper2) {
+  vector2 current = to_follow[step * sizeof(vector2)];
   stepper1.step(current[0]);
   stepper2.step(current[1]);
+}
+
+int ang_to_step(double ang) {
+  return (int) floor(((double) ang) * (4096.0 / 360.0));
+}
+
+path find_path(vector2 start_ang, vector2 target_ang) {
+  vector2 total_steps;
+  total_steps.x = ang_to_step(target_ang.x) - ang_to_step(start_ang.x);
+  total_steps.y = ang_to_step(target_ang.y) - ang_to_step(start_ang.y);
+
+  path found_path;
+  found_path.points = &total_steps;
+  found_path.length = 1;
+  return found_path;
 }
 
--- draw.h
+++ draw.h
@@ -19,8 +19,8 @@ vector2* get_interpolated_points(vector2 pos1, vector2 pos2);
 
 vector2 get_pos(vector2 ang, vector2 arm_len);
 
-void follow_path(vector2* path, int step, Stepper stepper1, Stepper stepper2);
+void follow_path(vector2* to_follow, int step, Stepper stepper1, Stepper stepper2);
 
-path* find_path(vector2 start_ang, vector2 target_ang);
+path find_path(vector2 start_ang, vector2 target_ang);
 
 #endif
